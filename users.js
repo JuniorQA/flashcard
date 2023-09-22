1b1ee42cc9b1bd6f18e9e06f813db92ceb493500
Igor_L <igor101188@gmail.com>
2023-09-22 12:09:46 +0300

    point+

--- users.js
+++ users.js
@@ -1,7 +1,17 @@
 class Users {
-  constructor(name, score = 0) {
+  constructor(name, score = 1000) {
     this.name = name;
     this.score = score;
+  }
+
+  // Метод увеличения очков
+  plusScore(point) {
+    this.score += point;
+  }
+
+  // Метод уменьшения очков
+  minusScore(point) {
+    this.score -= point;
   }
 }
 
